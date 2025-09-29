2a23f097fe72eff88756126a696a2367c100248b
Valdemar Larsen <shido813@gmail.com>
2025-09-29 13:03:18 +0200

    Test api

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Test() {
   const BASE_KEY_URL = import.meta.env.VITE_API_KEY;
 
+  const [location, setLocation] = useState(null);
+
   useEffect(() => {
     fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=roskilde&limit=11&appid=${BASE_KEY_URL}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=11&appid=${BASE_KEY_URL}`
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
-  }, []);
+  }, [location]);
 
   return (
     <>
